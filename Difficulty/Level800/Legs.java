e30ec6793b2962e934090b3ea91162ccc1b98917
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-03 00:32:23 +0530

    add solution - Legs div.3

---
+++ Difficulty/Level800/Legs.java
@@ -0,0 +1,40 @@
+package Difficulty.Level800;
+
+import java.util.Scanner;
+
+public class Legs {
+    public static void main(String [] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        if ((t >= 1) && (t <= 1000)) {
+
+            for (int i = 0; i < t; i++) {
+
+                int n = scanner.nextInt();
+
+                int count = 0;
+
+                if ((n >= 2) && (n <= 2000)) {
+
+                    if (n % 4 == 0) {
+                        count = n / 4;
+
+                    } else if ((n % 4) % 2 == 0) {
+                        count = (n / 4) + ((n % 4) / 2);
+                    } else if (n % 2 == 0) {
+                        count = n % 2;
+                    }
+
+
+                }
+
+                System.out.println(count);
+
+
+            }
+        }
+
+    }
+}
