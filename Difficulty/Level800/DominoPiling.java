070c12756bb26e3e17b774179b7a54000502c30b
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-03 00:50:38 +0530

    add solution - Domino Piling round 57

---
+++ Difficulty/Level800/DominoPiling.java
@@ -0,0 +1,20 @@
+//code by snua
+package Difficulty.Level800;
+
+import java.util.Scanner;
+
+public class DominoPiling {
+    public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        if((n>=1)&&(n<=16)&& (m>=1)&&(m<=16)){
+            int max = (m * n) / 2;
+
+            System.out.println(max);
+
+        }
+    }
+}
