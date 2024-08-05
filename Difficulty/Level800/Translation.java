fae73e5cbbf6a9f4050b9b4dd11bbea7501dceef
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-05 21:23:56 +0530

    add solution - Translation beta round 40 div2.

---
+++ Difficulty/Level800/Translation.java
@@ -0,0 +1,17 @@
+package Difficulty.Level800;
+//Code by senurah
+
+import java.util.Scanner;
+
+public class Translation {
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
+        StringBuilder s = new StringBuilder(scanner.nextLine());
+        String t = scanner.nextLine();
+        if(s.reverse().toString().equals(t)){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+    }
+}
