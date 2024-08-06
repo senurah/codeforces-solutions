ac49de6b913ddf7f584e226a91c65bde952274c8
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-06 22:44:15 +0530

    add solution - A+B again? round 964 div4.

---
+++ Difficulty/LevelUnrated/AplusBAgain.java
@@ -0,0 +1,21 @@
+package Difficulty.LevelUnrated;
+
+import java.util.Scanner;
+
+//code by senurah
+public class AplusBAgain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        scanner.nextLine();
+
+        if (t >= 1 && t <= 90) {
+            for(int i =0; i<t;i++){
+                String[] numbers = scanner.nextLine().split("");
+                System.out.println(Integer.valueOf(numbers[0])+Integer.valueOf(numbers[1]));
+            }
+
+        }
+
+    }
+}
