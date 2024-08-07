35f18eb13b44b71f375bdd3208409969c4ef155d
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-07 09:03:00 +0530

    add solution - Sum round 827 div4.

---
+++ Difficulty/Level800/Sum.java
@@ -0,0 +1,31 @@
+package Difficulty.Level800;
+//code by senurah
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        if (t >= 1 && t <= 9261) {
+            for (int i = 0; i < t; i++) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                int c = scanner.nextInt();
+
+
+                if((a+b)==c){
+                    System.out.println("YES");
+                }else if((a+c)==b){
+                    System.out.println("YES");
+                } else if ((b+c)==a) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+
+            }
+        }
+
+    }
+}
