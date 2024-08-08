fdcd6010c80cbf7f9620581a6e6262bef6376b2b
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-08 15:45:05 +0530

    add solution - StonesOnTheTable round 163 div2.

---
+++ Difficulty/Level800/StonesOnTheTable.java
@@ -0,0 +1,20 @@
+package Difficulty.Level800;
+//code by senurah
+import java.util.Scanner;
+
+public class StonesOnTheTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String stones = scanner.next();
+        int removeCount = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (stones.charAt(i) == stones.charAt(i + 1)) {
+                removeCount++;
+            }
+        }
+        System.out.println(removeCount);
+        scanner.close();
+    }
+}
+
