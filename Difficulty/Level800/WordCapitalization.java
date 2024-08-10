7f703e2942d73dadc4987c8b42bac34c2ce432f3
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-10 23:05:48 +0530

    add solution - Word Capitalization round 172 div2.

---
+++ Difficulty/Level800/WordCapitalization.java
@@ -0,0 +1,12 @@
+package Difficulty.Level800;
+//code by senurah
+import java.util.Scanner;
+
+public class WordCapitalization {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        System.out.println(word.substring(0, 1).toUpperCase() + word.substring(1));
+        scanner.close();
+    }
+}
