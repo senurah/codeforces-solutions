5bd2ad81e6fb18fd79117bcb1c3087abbe483748
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-18 11:53:47 +0530

    add solution - Football Beta round 77 div2.

---
+++ Difficulty/Level900/Football.java
@@ -0,0 +1,28 @@
+package Difficulty.Level900;
+//code by senurah
+import java.util.Scanner;
+
+public class Football {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] s = scanner.next().split("");
+        int count = 1;
+        boolean danger = false;
+        for(int i=1; i<s.length; i++){
+            if(s[i].equals(s[i-1])){
+                count++;
+                if(count == 7){
+                    danger = true;
+                    break;
+                }
+            }else{
+                count = 1;
+            }
+        }
+        if(danger){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
