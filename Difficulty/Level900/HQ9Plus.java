f876e3a3ba6c100e819febc1915681648db3e22e
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-22 18:58:59 +0530

    add solution - HQ9+ Beta round 96 div2.

---
+++ Difficulty/Level900/HQ9Plus.java
@@ -0,0 +1,16 @@
+package Difficulty.Level900;
+//code by senurah
+import java.util.Scanner;
+
+public class HQ9Plus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String program = scanner.next();
+        if(program.contains("H") || program.contains("Q") || program.contains("9")) {
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+
+    }
+}
