a430f95135aa3e7dd7da4d11081d0a27e606ee88
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-25 23:23:33 +0530

    add solution - Hulk round 366 div2.

---
+++ Difficulty/Level800/Hulk.java
@@ -0,0 +1,27 @@
+package Difficulty.Level800;
+//code by senurah
+import java.util.Scanner;
+public class Hulk {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                result.append("I hate");
+            } else {
+                result.append("I love");
+            }
+
+            if (i < n) {
+                result.append(" that ");
+            }
+        }
+
+        result.append(" it");
+        System.out.println(result);
+    }
+}
