e3110d49b83ab5260bd4489c51f66d76d1ba1cfc
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-27 23:28:20 +0530

    add solution -Nearly Lucky Number Beta round 84 div2.

---
+++ Difficulty/Level800/NearlyLuckyNumber.java
@@ -0,0 +1,21 @@
+package Difficulty.Level800;
+//code by senurah
+
+public class NearlyLuckyNumber {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String number = scanner.next();
+        int count = 0;
+        for (int i = 0; i < number.length(); i++) {
+            if (number.charAt(i) == '4' || number.charAt(i) == '7') {
+                count++;
+            }
+        }
+        if (count == 4 || count == 7) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        scanner.close();
+    }
+}
