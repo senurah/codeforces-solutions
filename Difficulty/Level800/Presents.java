1819299ca6e6592b75181c8cc79fa33dba5e2d71
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-08-31 21:43:45 +0530

    add solution -Present Beta round 97 div2.

---
+++ Difficulty/Level800/Presents.java
@@ -0,0 +1,21 @@
+package Difficulty.Level800;
+//code by senurah
+public class Presents {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int n = scanner.nextInt();
+        if(n>=1 && n <= 100){
+            int [] friends = new int[n];
+            for (int i =0 ; i<n ;  i++){
+                friends[i] = scanner.nextInt();
+            }
+            int [] presents = new int[n];
+            for(int i =0;i<n;i++){
+                presents[friends[i]-1] = i+1;
+            }
+            for(int i =0;i<n;i++){
+                System.out.print(presents[i]+" ");
+            }
+        }
+    }
+}
